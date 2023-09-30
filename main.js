08ba361639b3b6569a0a3e4add0c5d558d3515da
Andie Liu <andieliu@berkeley.edu>
2023-09-29 21:40:45 -0700

    add home page links styling and functionality

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', function () {  
+    const container = document.querySelector('body');
     
+    container.addEventListener('mousemove', (e) => {
+    // Get the mouse's X and Y coordinates
+    const mouseX = e.clientX / window.innerWidth;
+    const mouseY = e.clientY / window.innerHeight;
+
+    // Calculate RGB values based on mouse coordinates
+    const r = Math.floor(200 + mouseX * 10);
+    const g = Math.floor(200 + mouseY * 35);
+    const b = 230; // Adjust this value for the blue color
+
+    // Update the background color with the calculated RGB values
+    container.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    });
   
     // add class navbarDark on navbar scroll
     const menu = document.querySelector('.menu-container');
@@ -15,10 +29,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
   
-    const projLink = document.querySelector('.proj-link');
+    const projLink = document.getElementById('proj-link');
+    const expLink = document.getElementById('exp-link');
+
     projLink.addEventListener('click', function (event) {
         // Prevent the default behavior of the link (e.g., navigating to a different page)
         event.preventDefault();
+        expLink.classList.remove('active');
+        projLink.classList.add('active');
 
         // Get the target section element
         const targetSection = document.getElementById("projects");
@@ -32,12 +50,15 @@ document.addEventListener('DOMContentLoaded', function () {
             top: scrollPosition,
             behavior: 'smooth'
         });
+
     });
 
-    const expLink = document.querySelector('.exp-link');
     expLink.addEventListener('click', function (event) {
         // Prevent the default behavior of the link (e.g., navigating to a different page)
         event.preventDefault();
+        expLink.classList.add('active');
+        projLink.classList.remove('active');
+
         // Get the target section element
         const targetSection = document.getElementById("experience");
 
